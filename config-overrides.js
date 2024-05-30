f45f69b42cdb44060a22d3001cc384aa70d518a7
devloper-28 <pbadal392@gmail.com>
2024-05-30 18:40:44 +0530

    ini commit

---
+++ config-overrides.js
@@ -0,0 +1,24 @@
+{
+    "name": "your-react-app",
+    "version": "1.0.0",
+    "description": "A simple React.js project",
+    "main": "index.js",
+    "scripts": {
+      "start": "react-scripts start",
+      "build": "react-scripts build",
+      "test": "react-scripts test",
+      "eject": "react-scripts eject"
+    },
+    "keywords": [
+      "react",
+      "javascript"
+    ],
+    "author": "Your Name",
+    "license": "MIT",
+    "dependencies": {
+      "react": "^17.0.2",
+      "react-dom": "^17.0.2",
+      "react-scripts": "latest"
+    }
+  }
+  
